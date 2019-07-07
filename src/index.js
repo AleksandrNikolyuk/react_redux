6501329af1ec54d798a5e3a18394dcd0eb8e4bee
Aleksander Nikolyuk <anikolyk@gmail.com>
2019-07-07 19:10:11 +0300

    correct store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/MainPage/App';
+import App from './components/MainPage/index';
 import serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/store.js
+++ src/store.js
@@ -2,17 +2,11 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {};
-
-const middleware = [thunk];
+const storeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
 
 const store = createStore(
   rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  storeEnhancers(applyMiddleware(thunk)),
 );
 
 export default store;
